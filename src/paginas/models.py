1159a44366c2f9cba58aa6c117b3e79a795945ae
ORuan <ruanpablomp@gmail.com>
2020-06-19 02:54:11 -0300

    error: precisa de uma primary key com o force update

--- src/paginas/models.py
+++ src/paginas/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Paginas(models.Model):
-    dono = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    dono= models.CharField(max_length=200)
     titulo = models.CharField('Titulo', max_length=200)
     conteudo = models.TextField('Conteudo')
     data_criacao = models.DateTimeField(auto_now_add=True)
--- src/paginas/views.py
+++ src/paginas/views.py
@@ -35,30 +35,37 @@ def detalhes(request, id):
 def remove(request, id):
     context = {}
     context['pagina'] = Paginas.objects.get(pk=id) # consulte documentacao do django e veja como apagar... precisa fazer a rota - urls.py
-    
-    
-    
-
     if request.method =='POST':
         pagina_del = Paginas.objects.all()
         pagina_del.filter(id=id).delete()
 
     return render(request, 'remove.html', context=context)
 
+
 def editar(request, id):
     context = {}
-    pagina = Paginas.objects.get(id=id)
-    form_bd = CriarPaginaForms(instance=pagina)
-    form_nova = CriarPaginaForms(instance=pagina)
+    pagina = Paginas.objects.get(pk=id)
     context['pagina'] = pagina
+
+    form_bd = CriarPaginaForms(request.POST or None,instance=pagina)
     # implementar editar... parecido com criar...  
     # escrever a logica de negocios para salvar o item editado...
 
-    
-    if form_nova.is_valid():
-        form_bd = form_nova
+    #print(form_bd)
+    if form_bd.is_valid():
+        
+        titulo = form_bd.cleaned_data['titulo']
+        dono = form_bd.cleaned_data['dono'] 
+        conteudo = form_bd.cleaned_data['conteudo']
+        
+        
+        tarefa_nova = Paginas(titulo=titulo,dono=dono, conteudo=conteudo)
+        form_bd = tarefa_nova
         form_bd.save(force_update=True)
+        print('Sou o form bd',form_bd)
+    else:
+        print('falso')
 
-    context['form'] = form_nova
+    context['form'] = form_bd
 
     return render(request, 'editar.html', context=context)
